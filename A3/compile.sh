c55f98187aa046f09154faffeeccfd176c782381
vinaybhaskarrr <104855722+vinaybhaskarrr@users.noreply.github.com>
2024-03-23 22:47:46 +0530

    Add files via upload

---
+++ A3/compile.sh
@@ -0,0 +1,7 @@
+module load compiler/gcc/9.1.0
+module load compiler/python/3.6.0/ucs4/gnu/447
+module load pythonpackages/3.6.0/matplotlib/3.0.2/gnu
+module load pythonpackages/3.6.0/numpy/1.16.1/gnu
+module load pythonpackages/3.6.0/scikit-learn/0.21.2/gnu
+module load pythonpackages/3.6.0/pandas/0.23.4/gnu
+module load pythonpackages/3.6.0/scipy/1.1.0/gnu
